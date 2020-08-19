78fc539376f1b9dfc3b6b534e29b2302da220622
Joseph Mutua <Mutuaj793@gmail.com>
2020-08-19 20:30:10 +0300

    Add event acknowledgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,9 +6,14 @@ socket.on("message", (message) => {
 
 document.querySelector("#message-form").addEventListener("submit", (e) => {
   e.preventDefault();
-  const message = e.target.elements.message;
+  const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message Delivered!");
+  });
 });
 
 document.querySelector("#send-location").addEventListener("click", () => {
@@ -17,9 +22,15 @@ document.querySelector("#send-location").addEventListener("click", () => {
   }
 
   navigator.geolocation.getCurrentPosition((position) => {
-    socket.emit("sendLocation", {
-      latitude: position.coords.latitude,
-      longitude: position.coords.longitude,
-    });
+    socket.emit(
+      "sendLocation",
+      {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+      },
+      () => {
+        console.log("Location Shared!");
+      }
+    );
   });
 });
