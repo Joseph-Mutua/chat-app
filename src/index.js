c0a1a3383898337a5a4ce585cf8524eb9d13aafb
Joseph Mutua <Mutuaj793@gmail.com>
2020-08-20 19:16:06 +0300

    Render location  messages

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ io.on("connection", (socket) => {
 
   socket.on("sendLocation", (coords, callback) => {
     io.emit(
-      "message",
+      "locationMessage",
       `https://google.com/maps?q=${coords.latitude},${coords.longitude}`
     );
     callback();
@@ -41,6 +41,7 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     io.emit("message", "A user has Left");
   });
+  
 });
 
 server.listen(port, () => {
